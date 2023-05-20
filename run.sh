88411a459a37d3b03390f8a1a5a7185de2a1ae01
girirahayu <info@girirahayu.id>
2023-05-20 23:12:09 +0700

    tweak dockerfile

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --chdir /opt -w 2 --threads 2 -b 0.0.0.0:8000 --timeout 120 --access-logfile '-' 'app:http()'
+gunicorn --chdir /opt -w 2 --threads 8 -b 0.0.0.0:8000 --timeout 120 --access-logfile '-' 'app:http()'
